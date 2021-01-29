d108560e69313b5eecca06aa05b3aea7acda6146
karthikSingireddy <skarthikreddy189@gmail.com>
2021-01-28 19:22:02 -0800

    finished searchbar functionality

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppBar } from '@material-ui/core';
+import SearchBar from './components/SearchBar';
+
+function App() {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
