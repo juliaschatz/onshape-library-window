64e50596ba9d666feec869ae5e1f693d80fa4bde
Julia Schatz <julia.c.schatz@gmail.com>
2021-05-22 21:11:09 +0000

    Change redirect storing to use cookies instead of session keys

--- app.js
+++ app.js
@@ -123,13 +123,10 @@ function storeExtraParams(req, res) {
     redirect = "/application" + url.parse(req.url,true).search;
   }
 
-  var state = {
-    redirect: redirect
-  };
-
-  var stateString = JSON.stringify(state);
-  var uniqueID = "state" + req.sessionID;
-  client.set(uniqueID, stateString);
+  res.cookie("redirect", redirect, {
+    secure: true,
+    sameSite: 'none'
+  });
 
   return passport.authenticate("onshape")(req, res);
 }
@@ -143,21 +140,17 @@ app.use('/oauthRedirect',
     passport.authenticate('onshape', { failureRedirect: '/grantDenied' }),
     function(req, res) {
       var uniqueID = "state" + req.sessionID;
-      client.get(uniqueID, function(err, reply) {
-        // reply is null when the key is missing
-        if (reply != null) {
-          
-          var newParams = JSON.parse(reply);
-          res.redirect(newParams.redirect);
+      var redirect = req.cookies['redirect'];
+      if (redirect) {
+        res.redirect(redirect);
+      }
+      else {
+        var querystring = url.parse(req.url,true).search;
+        if (querystring === null || querystring === undefined) {
+          querystring = "";
         }
-        else {
-          var querystring = url.parse(req.url,true).search;
-          if (querystring === null || querystring === undefined) {
-            querystring = "";
-          }
-          res.redirect("/application" + querystring);
-        }
-      });
+        res.redirect("/application" + querystring);
+      }
     });
 
 /// catch 404 and forward to error handler
